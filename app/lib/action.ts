a9c9a2ba75f24a6ce9683c28f708d6fad7ae08e4
janoujan <janou@janou.pro>
2023-11-20 08:17:00 +0100

    close chapter 15 add metadata

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from '../../auth'
 
 const FormSchema = z.object({
   customerId: z.string({
@@ -113,5 +114,19 @@ export async function deleteInvoice(id: string) {
     return { message: "Invoice deleted" };
   } catch (error) {
     return { message: "Database error: failed to delete invoice" };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
   }
 }
